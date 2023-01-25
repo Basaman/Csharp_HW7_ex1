9feeb9298698bed0dc11fce13a3c8bdc8dc354a5
ilya <ilia.arkhangelsky@gmail.com>
2023-01-25 21:48:45 +0300

    Самозаполняемый двумерный вещественный массив

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿//Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+//m = 3, n = 4.
+//0,5 7 -2 -0,2
+//1 -3,3 8 -9,9
+//8 7,8 -7,1 9
+
+Console.Clear();
+
+Console.Write("Введите кол-во строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+
+Console.Write("Введите кол-во столбцов массива: ");
+int colums = int.Parse(Console.ReadLine() ?? "");
+
+Console.WriteLine();
+
+double[,] array = GetArray(rows, colums, -10, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    double AfterPointResult = 0;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+            AfterPointResult = new Random().NextDouble();
+            result[i, j] = result[i, j] + AfterPointResult;
+            result[i, j] = Math.Round(result[i, j], 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
